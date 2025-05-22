ac392ddc9805a311940fd4738543cdbdd70ff6b5
sypher-emi <emilio.imerico@gmail.com>
2025-05-22 11:59:05 +0200

    Esemipo 0.9.4

---
+++ EsemipoSirius2/EsemipoSirius/EsemipoSirius/Pages/ActivePower.cshtml.cs
@@ -0,0 +1,59 @@
+using EsemipoSirius.Database;
+using EsemipoSirius.Models;
+using EsemipoSirius.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EsemipoSirius.Pages
+{
+    public class ActivePowerModel : PageModel
+    {
+        ElencoDispositiviDisponibili dbDispositivi = new ElencoDispositiviDisponibili();
+        ActivePower dbActivePower = new ActivePower();
+        DBDispositiviLuoghi dbDis = new DBDispositiviLuoghi();
+        public List<DispositiviLuoghi> ElencoDispositivi { get; set; }
+        public List<ActivePowerDevice> dispositivoActivePower { get; set; }
+        public List<string> NomiDispositivi { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string NomeDevice { get; set; }
+
+        [BindProperty]
+        public List<DateTime?> Date { get; set; }
+        public List<float?> ActivePower { get; set; }
+        public List<string> Plant { get; set; }
+        public List<int> NumDisp { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public ActivePowerModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            dispositivoActivePower = new List<ActivePowerDevice>();
+            NomiDispositivi = new List<string>();
+            ElencoDispositivi = new List<DispositiviLuoghi>();
+            Date = new List<DateTime?>();
+            ActivePower = new List<float?>();
+            Plant = new List<string>();
+            NumDisp = new List<int>();
+        }
+
+        public void OnGet()
+        {
+            NomiDispositivi = dbDispositivi.DispositiviDisponibili();
+            ElencoDispositivi = dbDis.getNumDispositivi();
+            dispositivoActivePower = dbActivePower.getAll(NomeDevice);
+            foreach (DispositiviLuoghi a in ElencoDispositivi)
+            {
+                Plant.Add(a.Plant);
+                NumDisp.Add(a.NumDevice);
+            }
+            foreach (ActivePowerDevice a in dispositivoActivePower)
+            {
+                Date.Add(a.Date);
+                ActivePower.Add(a.ActivePower);
+            }
+
+        }
+    }
+}
--- EsemipoSirius2/EsemipoSirius/EsemipoSirius/obj/Debug/net8.0/EsemipoSirius.AssemblyInfo.cs
+++ EsemipoSirius2/EsemipoSirius/EsemipoSirius/obj/Debug/net8.0/EsemipoSirius.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsemipoSirius")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d67c25b3fbc5c32faa0cee877aeb06d565ba835f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d104d4a1fc4937c4e5a308fad1c35984ce0ef9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsemipoSirius")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsemipoSirius")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
