f95fdb23b33de703f70eecf84d31d47a692e7feb
Daniele Defilippi <daniele.defilippi06@gmail.com>
2025-05-22 18:12:15 +0200

    Esemipo 1.0

--- EsemipoSirius2/EsemipoSirius/EsemipoSirius/obj/Debug/net8.0/EsemipoSirius.AssemblyInfo.cs
+++ EsemipoSirius2/EsemipoSirius/EsemipoSirius/obj/Debug/net8.0/EsemipoSirius.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsemipoSirius")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d104d4a1fc4937c4e5a308fad1c35984ce0ef9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac392ddc9805a311940fd4738543cdbdd70ff6b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsemipoSirius")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsemipoSirius")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
